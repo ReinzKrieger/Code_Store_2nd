522024c188ff3f2ef787cb2ade07f38af1a5cc90
ReinzKrieger <62374195+ReinzKrieger@users.noreply.github.com>
2020-07-12 20:53:35 +0700

    upload database

---
+++ db_code.sql
@@ -0,0 +1,302 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Jul 2020 pada 15.51
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_code`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hapus_produk`
+--
+
+CREATE TABLE `hapus_produk` (
+  `id_produk` int(10) DEFAULT NULL,
+  `nama_produk` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  `harga` varchar(10) DEFAULT NULL,
+  `gambar` varchar(50) DEFAULT NULL,
+  `kategori` int(10) DEFAULT NULL,
+  `tgl_hapus` date DEFAULT NULL,
+  `user_cust` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `hapus_produk`
+--
+
+INSERT INTO `hapus_produk` (`id_produk`, `nama_produk`, `deskripsi`, `harga`, `gambar`, `kategori`, `tgl_hapus`, `user_cust`) VALUES
+(1, 'Baju Pria Merah', 'Kondisi Baik', '200000', '1.jpg', 1, '2020-07-12', 'root@localhost'),
+(2, 'Baju Pria Hitam Putih', 'Kondisi Baik', '250000', '2.jpg', 1, '2020-07-12', 'root@localhost'),
+(3, 'Baju Pria Lengan Panjang', 'Kondisi Baik', '300000', '3.jpg', 1, '2020-07-12', 'root@localhost'),
+(4, 'Celana Panjang Pria Coklat', 'Kondisi Baik', '300000', '4.jpg', 2, '2020-07-12', 'root@localhost'),
+(5, 'Celana Panjang Pria Abu - abu', 'Kondisi Baik', '250000', '5.jpg', 2, '2020-07-12', 'root@localhost'),
+(6, 'Celana Panjang Pria Hitam', 'Kondisi Baik', '400000', '6.jpg', 2, '2020-07-12', 'root@localhost'),
+(7, 'Sepatu Converse Hitam', 'Kondisi Baik', '500000', '7.jpg', 3, '2020-07-12', 'root@localhost'),
+(8, 'Sepatu Converse Kuning', 'Kondisi Baik', '450000', '8.jpg', 3, '2020-07-12', 'root@localhost'),
+(9, 'Sepatu Converse Putih', 'Kondisi Baik', '700000', '9.jpg', 3, '2020-07-12', 'root@localhost');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_detail_order`
+--
+
+CREATE TABLE `tbl_detail_order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(10) DEFAULT NULL,
+  `produk` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `harga` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_kategori` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id`, `nama_kategori`) VALUES
+(1, 'Perabotan'),
+(2, 'Lampu'),
+(3, 'Kipas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `pelanggan` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pelanggan`
+--
+
+CREATE TABLE `tbl_pelanggan` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(10) UNSIGNED NOT NULL,
+  `nama_produk` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  `harga` varchar(10) DEFAULT NULL,
+  `gambar` varchar(50) DEFAULT NULL,
+  `kategori` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `nama_produk`, `deskripsi`, `harga`, `gambar`, `kategori`) VALUES
+(5, 'Kipas angin 1', 'Sangat Bagus', '350000', '5f0acc1c8f0ec.jpg', 3),
+(5, 'Kipas angin 2', 'Kipas Angin Bagus', '250000', '5f0ad38798006.jpg', 3),
+(5, 'Kipas angin 3', 'Kipas angin bagus sekali', '270000', '5f0ad3bbd4b82.jpg', 3);
+
+--
+-- Trigger `tbl_produk`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus_barang` AFTER DELETE ON `tbl_produk` FOR EACH ROW begin
+insert into hapus_produk (id_produk, nama_produk, deskripsi, harga, gambar, kategori, tgl_hapus, user_cust) values (old.id_produk, old.nama_produk, old.deskripsi, old.harga, old.gambar, old.kategori, sysdate(), current_user);
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `role_id`, `is_active`) VALUES
+(1, 'Admin', 'admin@admin.com', 'admin', '0192023a7bbd73250516f069df18b500', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users_role`
+--
+
+CREATE TABLE `users_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_cust`
+--
+
+CREATE TABLE `user_cust` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_cust`
+--
+
+INSERT INTO `user_cust` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
+(8, 'Fadhil Hanif', 'dhilhanif@gmail.com', 'dhilhanif', '12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `users_role`
+--
+ALTER TABLE `users_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_cust`
+--
+ALTER TABLE `user_cust`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `users_role`
+--
+ALTER TABLE `users_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_cust`
+--
+ALTER TABLE `user_cust`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
